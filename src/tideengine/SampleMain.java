11720f2e91652bf664917ab63efac0f60d2e6247
olivier.lediouris <olivier.lediouris@b0844235-2bdb-4079-cde7-3694a32630e6>
2012-09-26 19:17:50 +0000

    Removed the re-generation from the BackEndXMLTideComputer. It's now in RegenerateXMLData.java.

--- src/tideengine/SampleMain.java
+++ src/tideengine/SampleMain.java
@@ -34,11 +34,8 @@ public class SampleMain
         String harmonicName = "harmonics_06_14_2004_fr.txt";
   //    String harmonicName1 = "harmonics-dwf-20110410-free.txt";
   //    String harmonicName2 = "harmonics-dwf-20110410-nonfree.txt";
-        BackEndXMLTideComputer.setVerbose(true);
         long before = System.currentTimeMillis();
-        try { BackEndXMLTideComputer.generateXML(harmonicName); } catch (Exception ex) { ex.printStackTrace(); }
-  //    try { BackEndTideComputer.generateXML(new String[] { harmonicName1, 
-  //                                                         harmonicName2 }); } catch (Exception ex) { ex.printStackTrace(); }
+        try { RegenerateXMLData.generateXML(harmonicName); } catch (Exception ex) { ex.printStackTrace(); }
         long after = System.currentTimeMillis();
         System.out.println("It took " + Long.toString(after - before) + " ms to rebuild the XML files.");
       }
